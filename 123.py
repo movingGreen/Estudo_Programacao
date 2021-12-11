b4ecd37333f2b29bcf956384b3a111eda7de456a
Elizeu <el_vailant@hotmail.com>
2021-12-11 19:37:36 -0300

    randon function

--- 123.py
+++ 123.py
@@ -17,4 +17,16 @@ def testeNumpy() :
     print("Dia da semana " + X.strftime("%u"))
 
 
-testeNumpy()
+def func (x, y) :
+    
+    if y :
+        print("tem y")
+        return
+    if (y == 0) :
+        return 0
+    elif (y == 1) :
+        return x
+    else :
+        return x + func (x, y-1)
+    
+print(func(14, 1))
