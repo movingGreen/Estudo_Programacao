53f26d1f678ecdd431489f9de56cfab8cf250f86
Elizeu <el_vailant@hotmail.com>
2022-06-13 21:24:38 -0300

    13.06.2022

--- Estudo_JavaScript/JavaScript_info/objects.js
+++ Estudo_JavaScript/JavaScript_info/objects.js
@@ -5,9 +5,9 @@ user.surname = "Smith";
 user.name = "Pete";
 delete user.name;
 
-for (let chave in user) {
-    alert( user[chave] );
-}
+// for (let chave in user) {
+//     alert( user[chave] );
+// }
 
 function isEmpty(objeto) {
     for (let chave in objeto) {
@@ -18,8 +18,44 @@ function isEmpty(objeto) {
 
 let schedule = {};
 
-alert( isEmpty(schedule) ); // true
+// alert( isEmpty(schedule) ); // true
 
 schedule["8:30"] = "get up";
 
-alert( isEmpty(schedule) ); // false
+// alert( isEmpty(schedule) ); // false
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+function somaDeSalarios(salarios) {
+    sum = 0;
+    for (let valor in salarios) {
+        sum += salarios[valor];
+    }
+    return sum;
+}
+
+// alert( somaDeSalarios(salaries) );
+console.log( somaDeSalarios(salaries) );
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(menu) {
+    for (const item in menu) {
+        if (typeof menu[item] == "number") {
+            menu[item] *= 2;
+        }
+    }
+}
+
+// console.log(menu);
+// multiplyNumeric(menu);
+// console.log(menu);
+
