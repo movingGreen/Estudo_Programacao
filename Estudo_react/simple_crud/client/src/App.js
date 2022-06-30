539a9b78ace3216a9b256973f3afdb182b352655
Elizeu <el_vailant@hotmail.com>
2022-06-29 22:42:25 -0300

    campos

--- Estudo_react/simple_crud/client/src/App.js
+++ Estudo_react/simple_crud/client/src/App.js
@@ -2,7 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Olá mundo!</div>
+    <div className="App">
+      <form>
+        <label for="nome">Nome do funcionario:</label>
+        <input type="text" name="nome"></input>
+        <br/><br/>
+        <label for="idade">Valor do salário</label>
+        <input type="number" name="idade"></input>
+        <br/><br/>
+        <label for="salario">Valor do salário</label>
+        <input type="number" name="salario"></input>
+        <br/><br/>
+        <label for="salario">Valor do salário</label>
+        <input type="number" name="salario"></input>
+      </form>
+    </div>
   );
 }
 
